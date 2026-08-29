0a91600e34954c912dd35bf6f75e934944a09de5
Christopher C Taylor <66293653+FairInHeight@users.noreply.github.com>
2026-08-29 10:38:43 -0400

    Support multiple source file arguments

--- compiler/include/args.hpp
+++ compiler/include/args.hpp
@@ -2,10 +2,11 @@
 #define CSAM_ARGS_HPP
 
 #include <string>
+#include <vector>
 
 struct Arguments {
     bool debug = false;
-    std::string filepath;
+    std::vector<std::string> filepaths;
 };
 
 Arguments parse_arguments(int argc, char* argv[]);
