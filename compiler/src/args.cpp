4f4192c04634875d91ad2eb2001f506751865ef9
Christopher C Taylor <66293653+FairInHeight@users.noreply.github.com>
2026-08-29 10:38:50 -0400

    Allow unlimited additional source filepaths

--- compiler/src/args.cpp
+++ compiler/src/args.cpp
@@ -9,7 +9,7 @@ namespace {
 
 void print_syntax(const char* program)
 {
-    std::cerr << "Syntax: " << program << " [-flags] <filepath>\n";
+    std::cerr << "Syntax: " << program << " [-flags] <filepath> [filepath ...]\n";
 }
 
 void print_valid_flags()
@@ -22,50 +22,49 @@ void print_valid_flags()
 
 Arguments parse_arguments(int argc, char* argv[])
 {
-    if (argc < 2 || argc > 3) {
+    if (argc < 2) {
         print_syntax(argv[0]);
-        throw std::runtime_error("invalid argument count");
+        throw std::runtime_error("missing filepath");
     }
 
     Arguments arguments;
     const std::string first_argument = argv[1];
+    int first_filepath = 1;
 
-    // No flag: argv[1] is the filepath.
+    // No flags: argv[1] is the first filepath and every following argument
+    // is an additional filepath.
     if (first_argument.empty() || first_argument[0] != '-') {
-        if (argc != 2) {
+        first_filepath = 1;
+    } else {
+        // Flags are restricted to argv[1]. All following arguments are paths.
+        for (std::size_t i = 1; i < first_argument.size(); ++i) {
+            switch (first_argument[i]) {
+                case 'd':
+                    arguments.debug = true;
+                    break;
+
+                default:
+                    std::cerr << "Invalid flag: -" << first_argument[i] << '\n';
+                    print_valid_flags();
+                    throw std::invalid_argument("invalid flag");
+            }
+        }
+
+        first_filepath = 2;
+
+        if (argc < 3) {
             print_syntax(argv[0]);
-            throw std::runtime_error("invalid argument count");
-        }
-
-        arguments.filepath = first_argument;
-        csam_debug = false;
-        return arguments;
-    }
-
-    // Flag mode: argv[1] contains flags and argv[2] is the filepath.
-    if (argc != 3) {
-        print_syntax(argv[0]);
-        throw std::runtime_error("missing filepath");
-    }
-
-    for (std::size_t i = 1; i < first_argument.size(); ++i) {
-        switch (first_argument[i]) {
-            case 'd':
-                arguments.debug = true;
-                break;
-
-            default:
-                std::cerr << "Invalid flag: -" << first_argument[i] << '\n';
-                print_valid_flags();
-                throw std::invalid_argument("invalid flag");
+            throw std::runtime_error("missing filepath");
         }
     }
 
-    arguments.filepath = argv[2];
+    for (int i = first_filepath; i < argc; ++i) {
+        if (std::string(argv[i]).empty()) {
+            print_syntax(argv[0]);
+            throw std::runtime_error("empty filepath");
+        }
 
-    if (arguments.filepath.empty()) {
-        print_syntax(argv[0]);
-        throw std::runtime_error("missing filepath");
+        arguments.filepaths.emplace_back(argv[i]);
     }
 
     csam_debug = arguments.debug;
