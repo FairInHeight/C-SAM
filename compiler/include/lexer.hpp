b2b8498f7f91925ac1fe5e7eba5d494c25e529a4
Christopher C Taylor <66293653+FairInHeight@users.noreply.github.com>
2026-08-31 20:56:29 -0400

    portability: make lexer path handling OS agnostic

--- compiler/include/lexer.hpp
+++ compiler/include/lexer.hpp
@@ -2,18 +2,20 @@
 #define CSAM_LEXER_HPP
 
 #include "token.hpp"
+
+#include <filesystem>
 #include <string>
 #include <vector>
 
 class Lexer {
 public:
-    Lexer(const std::string& source, const std::string& filepath);
+    Lexer(const std::string& source, const std::filesystem::path& filepath);
 
     std::vector<Token> tokenize();
 
 private:
     const std::string& source;
-    const std::string& filepath;
+    std::filesystem::path filepath;
 };
 
 #endif
