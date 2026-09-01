420fdd60b50d2da707a5c53ea86a0941522c8c41
Christopher C Taylor <66293653+FairInHeight@users.noreply.github.com>
2026-08-31 20:56:24 -0400

    portability: use filesystem paths for arguments

--- compiler/include/args.hpp
+++ compiler/include/args.hpp
@@ -1,12 +1,13 @@
 #ifndef CSAM_ARGS_HPP
 #define CSAM_ARGS_HPP
 
+#include <filesystem>
 #include <string>
 #include <vector>
 
 struct Arguments {
     bool debug = false;
-    std::vector<std::string> filepaths;
+    std::vector<std::filesystem::path> filepaths;
 };
 
 Arguments parse_arguments(int argc, char* argv[]);
