fac67b17d2eb9c7aa452c09b6a0d46d0c79abc5c
Christopher C Taylor <66293653+FairInHeight@users.noreply.github.com>
2026-09-01 03:14:57 -0400

    lexer: expand token vocabulary for CSS syntax

--- compiler/include/token.hpp
+++ compiler/include/token.hpp
@@ -15,9 +15,14 @@ enum class TokenType {
     Identifier,
     String,
     Number,
+    Percent,
+    Hash,
+    AtKeyword,
 
     Colon,
     Semicolon,
+    Comma,
+    Equals,
     LeftBrace,
     RightBrace,
     LeftAngle,
@@ -26,10 +31,29 @@ enum class TokenType {
     RightBracket,
     LeftParen,
     RightParen,
-    Equals,
 
-    Hash,
-    Comma,
+    Plus,
+    Minus,
+    Asterisk,
+    Slash,
+    GreaterThan,
+    LessThan,
+    Tilde,
+    Pipe,
+    Caret,
+    Dollar,
+    Ampersand,
+    Bang,
+    QuestionMark,
+    Dot,
+    Backslash,
+
+    IncludesMatch,
+    DashMatch,
+    PrefixMatch,
+    SuffixMatch,
+    SubstringMatch,
+    Column,
 
     EndOfFile
 };
@@ -40,8 +64,13 @@ inline const char* token_type_name(TokenType type)
         case TokenType::Identifier: return "Identifier";
         case TokenType::String: return "String";
         case TokenType::Number: return "Number";
+        case TokenType::Percent: return "Percent";
+        case TokenType::Hash: return "Hash";
+        case TokenType::AtKeyword: return "AtKeyword";
         case TokenType::Colon: return "Colon";
         case TokenType::Semicolon: return "Semicolon";
+        case TokenType::Comma: return "Comma";
+        case TokenType::Equals: return "Equals";
         case TokenType::LeftBrace: return "LeftBrace";
         case TokenType::RightBrace: return "RightBrace";
         case TokenType::LeftAngle: return "LeftAngle";
@@ -50,9 +79,27 @@ inline const char* token_type_name(TokenType type)
         case TokenType::RightBracket: return "RightBracket";
         case TokenType::LeftParen: return "LeftParen";
         case TokenType::RightParen: return "RightParen";
-        case TokenType::Equals: return "Equals";
-        case TokenType::Hash: return "Hash";
-        case TokenType::Comma: return "Comma";
+        case TokenType::Plus: return "Plus";
+        case TokenType::Minus: return "Minus";
+        case TokenType::Asterisk: return "Asterisk";
+        case TokenType::Slash: return "Slash";
+        case TokenType::GreaterThan: return "GreaterThan";
+        case TokenType::LessThan: return "LessThan";
+        case TokenType::Tilde: return "Tilde";
+        case TokenType::Pipe: return "Pipe";
+        case TokenType::Caret: return "Caret";
+        case TokenType::Dollar: return "Dollar";
+        case TokenType::Ampersand: return "Ampersand";
+        case TokenType::Bang: return "Bang";
+        case TokenType::QuestionMark: return "QuestionMark";
+        case TokenType::Dot: return "Dot";
+        case TokenType::Backslash: return "Backslash";
+        case TokenType::IncludesMatch: return "IncludesMatch";
+        case TokenType::DashMatch: return "DashMatch";
+        case TokenType::PrefixMatch: return "PrefixMatch";
+        case TokenType::SuffixMatch: return "SuffixMatch";
+        case TokenType::SubstringMatch: return "SubstringMatch";
+        case TokenType::Column: return "Column";
         case TokenType::EndOfFile: return "EndOfFile";
     }
 
