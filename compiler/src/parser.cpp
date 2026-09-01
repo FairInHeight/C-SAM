b535454d31e6af84b6ca456578ee1dbaeb1d683f
Christopher C Taylor <66293653+FairInHeight@users.noreply.github.com>
2026-09-01 05:58:06 -0400

    refactor: factor value parsing out of parser

--- compiler/src/parser.cpp
+++ compiler/src/parser.cpp
@@ -1,4 +1,5 @@
 #include "parser.hpp"
+#include "value_parser.hpp"
 
 #include <sstream>
 #include <stdexcept>
@@ -328,106 +329,6 @@ void Parser::parse_property()
 std::vector<std::unique_ptr<ValueNode>> Parser::parse_value(const char* context)
 {
     (void)context;
-    std::vector<std::unique_ptr<ValueNode>> values;
-
-    while (!check(TokenType::Semicolon) && !check(TokenType::EndOfFile)) {
-        values.push_back(parse_single_value());
-    }
-
-    if (values.empty()) {
-        unexpected(peek(), "Expected value");
-    }
-
-    return values;
-}
-
-std::unique_ptr<ValueNode> Parser::parse_function()
-{
-    const Token& name = consume(TokenType::Identifier, "Expected function name");
-    consume(TokenType::LeftParen, "Expected '(' after function name");
-
-    std::vector<FunctionValueNode::Argument> arguments;
-
-    if (check(TokenType::RightParen)) {
-        advance();
-        return std::make_unique<FunctionValueNode>(name, name.value, std::move(arguments));
-    }
-
-    while (true) {
-        arguments.push_back(parse_function_argument());
-
-        if (check(TokenType::RightParen)) {
-            advance();
-            break;
-        }
-
-        consume(TokenType::Comma, "Expected ',' or ')' in function arguments");
-        if (check(TokenType::RightParen)) {
-            unexpected(peek(), "Expected function argument after ','");
-        }
-    }
-
-    return std::make_unique<FunctionValueNode>(name, name.value, std::move(arguments));
-}
-
-FunctionValueNode::Argument Parser::parse_function_argument()
-{
-    FunctionValueNode::Argument values;
-
-    while (!check(TokenType::Comma) && !check(TokenType::RightParen)) {
-        if (check(TokenType::EndOfFile) || check(TokenType::Semicolon)) {
-            unexpected(peek(), "Expected ')' after function arguments");
-        }
-
-        values.push_back(parse_single_value());
-    }
-
-    if (values.empty()) {
-        unexpected(peek(), "Expected function argument");
-    }
-
-    return values;
-}
-
-std::unique_ptr<ValueNode> Parser::parse_single_value()
-{
-    const Token& token = peek();
-
-    if (token.type == TokenType::Identifier &&
-        current + 1 < tokens.size() &&
-        tokens[current + 1].type == TokenType::LeftParen &&
-        token.location.line == tokens[current + 1].location.line &&
-        token.location.column + token.value.size() == tokens[current + 1].location.column) {
-        return parse_function();
-    }
-
-    if (token.type == TokenType::Number) {
-        const Token& number = advance();
-
-        if (check(TokenType::Percent)) {
-            const Token& percent = peek();
-            if (number.location.line == percent.location.line &&
-                number.location.column + number.value.size() == percent.location.column) {
-                advance();
-                return std::make_unique<PercentageValueNode>(number, number.value);
-            }
-        }
-
-        if (check(TokenType::Identifier)) {
-            const Token& unit = peek();
-            if (number.location.line == unit.location.line &&
-                number.location.column + number.value.size() == unit.location.column) {
-                advance();
-                return std::make_unique<DimensionValueNode>(number, number.value, unit.value);
-            }
-        }
-
-        return std::make_unique<NumberValueNode>(number, number.value);
-    }
-
-    if (token.type == TokenType::String) {
-        return std::make_unique<StringValueNode>(advance(), token.value);
-    }
-
-    return std::make_unique<RawValueNode>(advance(), token.value);
+    ValueParser value_parser(tokens, current);
+    return value_parser.parse_value();
 }
