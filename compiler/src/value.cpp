e2bbbcf337149e463bf63466ca3197bcb84cd30e
Christopher C Taylor <66293653+FairInHeight@users.noreply.github.com>
2026-09-01 04:59:18 -0400

    ast: implement semantic CSS value nodes

---
+++ compiler/src/value.cpp
@@ -0,0 +1,31 @@
+#include "value.hpp"
+
+#include <utility>
+
+NumberValueNode::NumberValueNode(const Token& token, std::string representation)
+    : ValueNode(ASTNodeType::NumberValue, token),
+      representation(std::move(representation))
+{
+}
+
+DimensionValueNode::DimensionValueNode(
+    const Token& token,
+    std::string number,
+    std::string unit)
+    : ValueNode(ASTNodeType::DimensionValue, token),
+      numeric_value(std::move(number)),
+      unit_value(std::move(unit))
+{
+}
+
+PercentageValueNode::PercentageValueNode(const Token& token, std::string number)
+    : ValueNode(ASTNodeType::PercentageValue, token),
+      numeric_value(std::move(number))
+{
+}
+
+StringValueNode::StringValueNode(const Token& token, std::string value)
+    : ValueNode(ASTNodeType::StringValue, token),
+      string_value(std::move(value))
+{
+}
